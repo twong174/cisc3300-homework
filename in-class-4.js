5c861e8401a061ac67637212c2283425f50f0cb6
twong174 <156949430+twong174@users.noreply.github.com>
2024-09-25 16:13:47 -0400

    Add files via upload

---
+++ in-class-4.js
@@ -0,0 +1,12 @@
+console.log('in-class-4.js loaded!');
+
+const myArray = [1, 2, 3, 4, 5]
+
+for (let i = 0; i < myArray.length; i++) { 
+  if (i % 2 === 0) {
+    console.log(`${i} is even!`)
+  } else { 
+    console.log(`${i} is odd!`)
+  }
+}
+
